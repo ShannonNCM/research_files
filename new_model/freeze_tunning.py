bea4aa2be4691746729b59f118fef7c75cad449b
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2026-06-05 16:22:50 +0900

    update freeze tunning

--- new_model/freeze_tunning.py
+++ new_model/freeze_tunning.py
@@ -15,8 +15,8 @@ os.makedirs('test_res', exist_ok=True) #creates a folder to store the files of t
 import functions as f #import functions used in this notebook
 
 #setting the loop for running
-#epoch_values = {40, 60, 90, 100, 120}
-epoch_values = {1,2}
+epoch_values = {40, 60, 80, 90, 100, 120}
+#epoch_values = {1,2}
 
 for epochs in epoch_values:
     ###############################################################
